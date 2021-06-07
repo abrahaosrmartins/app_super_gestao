8c4c9d2ea24494092fa6c94abcd36aae41e47f49
Abrahão Martins <abrahaosrmartins@gmail.com>
2021-06-07 20:54:14 -0300

    First Commit

---
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -0,0 +1,16 @@
+<div class="topo">
+
+    <div class="logo">
+        <a href="{{route('site.index')}}">
+            <img src="img/logo.png">
+        </a>
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="{{ route('site.index') }}">Home</a></li>
+            <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+            <li><a href="{{ route('site.contato') }}">Contato</a></li>
+        </ul>
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('sobre_nos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
