f1407e9efc1c7deb6450e71c0021bc4880ff91b6
Abrahão Martins <abrahaosrmartins@gmail.com>
2021-07-03 22:34:59 -0300

    [Refactor] Changed Route sintax

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 Route::get('sobre_nos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
