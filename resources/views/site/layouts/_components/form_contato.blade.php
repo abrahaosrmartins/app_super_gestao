45181491dad5f39352820ff5e66994957bef08bd
Abrahão Martins <abrahaosrmartins@gmail.com>
2021-07-25 19:56:58 -0300

    [Refactor] Changed contato form name call - Changed motivo_contato to motivo_contatos_id

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -9,10 +9,10 @@
     <br>
 
 
-    <select name="motivo_contato" class="{{ $classe }}">
+    <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         @foreach ($motivo_contatos as $motivo_contato)
-            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>
+            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>
                 {{ $motivo_contato->motivo_contato }} </option>
         @endforeach
     </select>
