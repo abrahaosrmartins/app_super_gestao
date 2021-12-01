7e42b5c5296b613caa5e8bd3dd6d579b7476937d
Abrahão Martins <abrahaosrmartins@gmail.com>
2021-12-01 20:48:16 -0300

    [Fix] Formated index file

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -10,14 +10,14 @@
 
     <div class="menu">
         <ul>
-            <li><a href="{{route('app.fornecedor.adicionar')}}">Novo</a></li>
-            <li><a href="{{route('app.fornecedor')}}">Consulta</a></li>
+            <li><a href="{{ route('app.fornecedor.adicionar') }}">Novo</a></li>
+            <li><a href="{{ route('app.fornecedor') }}">Consulta</a></li>
         </ul>
     </div>
 
     <div class="informacao-pagina">
         <div style="width: 30%; margin-left: auto; margin-right: auto">
-            <form method="post" action="{{route('app.fornecedor.listar')}}">
+            <form method="post" action="{{ route('app.fornecedor.listar') }}">
                 @csrf
                 <input type="text" name="nome" placeholder="Nome" class="borda-preta">
                 <input type="text" name="site" placeholder="Site" class="borda-preta">
