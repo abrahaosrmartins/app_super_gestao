cc6722157e98991c813433c215cd388f971d1161
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-05 00:54:20 -0300

    [Style] Reformated index blade

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -19,28 +19,28 @@
         <div style="width: 90%; margin-left: auto; margin-right: auto">
             <table border="1" width="100%">
                 <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Site</th>
-                        <th>UF</th>
-                        <th>E-mail</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
+                <tr>
+                    <th>Nome</th>
+                    <th>Site</th>
+                    <th>UF</th>
+                    <th>E-mail</th>
+                    <th></th>
+                    <th></th>
+                </tr>
                 </thead>
 
                 <tbody>
-                    @foreach ($fornecedores as $fornecedor)
-                        <tr>
-                            <td>{{ $fornecedor->nome }}</td>
-                            <td>{{ $fornecedor->site }}</td>
-                            <td>{{ $fornecedor->uf }}</td>
-                            <td>{{ $fornecedor->email }}</td>
-                            <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
-                            <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
-                            {{-- sempre que clicamos sobre um link, acionamos uma requisição via get --}}
-                        </tr>
-                    @endforeach
+                @foreach ($fornecedores as $fornecedor)
+                    <tr>
+                        <td>{{ $fornecedor->nome }}</td>
+                        <td>{{ $fornecedor->site }}</td>
+                        <td>{{ $fornecedor->uf }}</td>
+                        <td>{{ $fornecedor->email }}</td>
+                        <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
+                        <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
+                        {{-- sempre que clicamos sobre um link, acionamos uma requisição via get --}}
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
              {{ $fornecedores->appends($request)->links() }}
