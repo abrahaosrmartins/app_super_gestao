cb41795bd6728e947a3e28da6264250619a54465
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-06 18:34:44 -0300

    [Refactor] Changed unidades table fields order

--- database/migrations/2021_06_19_020358_create_unidades_table.php
+++ database/migrations/2021_06_19_020358_create_unidades_table.php
@@ -21,13 +21,13 @@ class CreateUnidadesTable extends Migration
         });
 
         //adicionar relacionamento com a tabela produtos
-        Schema::table('produtos', function(Blueprint $table){
-            $table->unsignedBigInteger('unidade_id');
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id')->after('peso');
             $table->foreign('unidade_id')->references('id')->on('unidades');
         });
-        
+
         // adicionar relacionamento com a tabela produto_detalhes
-        Schema::table('produto_detalhes', function(Blueprint $table){
+        Schema::table('produto_detalhes', function (Blueprint $table) {
             $table->unsignedBigInteger('unidade_id');
             $table->foreign('unidade_id')->references('id')->on('unidades');
         });
