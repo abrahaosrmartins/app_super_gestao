3c6338ffe033ff6f630c2f9f20cad9548927566b
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-08 18:59:05 -0300

    [Feature] Added hasOne relationship

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Produto extends Model
 {
@@ -15,4 +16,12 @@ class Produto extends Model
         'peso',
         'unidade_id'
     ];
+
+    /**
+     * @return HasOne
+     */
+    public function produtoDetalhe(): HasOne
+    {
+        return $this->hasOne(ProdutoDetalhe::class);
+    }
 }
