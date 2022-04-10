b0aadf483ecb97008f65de4c6415d9652b0224a3
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-10 00:05:00 -0300

    [Feature] Added Pedido controller

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Models\Pedido;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return View
+     */
+    public function index(Request $request): View
+    {
+        $pedidos = Pedido::paginate(10);
+        return view('app.pedido.index', ['pedidos' => $pedidos, 'request' => $request->all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        $clientes = Cliente::all();
+        return view('app.pedido.create', ['clientes' => $clientes]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $regras = [
+            'cliente_id' => 'exists:clientes,id'
+        ];
+        $feedback = [
+            'cliente_id.exists' => 'O cliente informado não existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        // você pode instanciar o objeto Pedido aqui ou passa-lo como parâmetro do método lá em cima
+        $pedido = new Pedido();
+        $pedido->cliente_id = $request->get('cliente_id');
+        $pedido->save();
+
+        return redirect()->route('pedido.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     */
+    public function show(int $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     */
+    public function edit(int $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     */
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     */
+    public function destroy(int $id)
+    {
+        //
+    }
+}
