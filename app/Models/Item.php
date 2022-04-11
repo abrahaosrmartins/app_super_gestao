64cc6ae66a1d9b777cab6051e120433ba9997984
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-10 22:33:00 -0300

    [Feature] Added belongsToMany to item produto

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Item extends Model
@@ -36,5 +37,24 @@ class Item extends Model
     public function fornecedor(): BelongsTo
     {
         return $this->belongsTo(Fornecedor::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function pedidos(): BelongsToMany
+    {
+        //usando nomes padronizados
+//        return $this->belongsToMany(Pedido::class, 'pedidos_produtos');
+        // juntamente com o model, passamos a tabela auxiliar
+
+        //usando nomes não padronizados
+        return $this->belongsToMany(Pedido::class, 'pedidos_produtos', 'produto_id', 'pedido_id');
+        /*
+            1 - Modelo do relacionamento NxN em relação o Modelo que estamos implementando
+            2 - É a tabela auxiliar que armazena os registros de relacionamento
+            3 - Representa o nome da FK da tabela mapeada pelo modelo na tabela de relacionamento
+            4 - Representa o nome da FK da tabela mapeada pelo modelo utilizado no relacionamento que estamos implementando
+        */
     }
 }
