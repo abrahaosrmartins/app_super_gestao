345a9c99361b3039f845e445586dd50a4dd70451
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-10 22:33:25 -0300

    [Feature] Added belongsToMany to pedido model

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -12,17 +12,18 @@ class Pedido extends Model
 
     public function produtos()
     {
-        //usando nomes padronizados
-        // return $this->belongsToMany(Produto::class, 'pedidos_produtos');
-        // juntamente com o model, passamos a tabela auxiliar
-
-        //usando nomes não padronizados
-        return $this->belongsToMany(Item::class, 'pedidos_produtos', 'pedido_id', 'produto_id');
+        // usando nomes não padronizados
+        return $this->belongsToMany(Item::class, 'pedidos_produtos', 'pedido_id', 'produto_id')
+            ->withPivot('id', 'created_at', 'updated_at', 'quantidade');
         /*
             1 - Modelo do relacionamento NxN em relação o Modelo que estamos implementando
             2 - É a tabela auxiliar que armazena os registros de relacionamento
             3 - Representa o nome da FK da tabela mapeada pelo modelo na tabela de relacionamento
             4 - Representa o nome da FK da tabela mapeada pelo modelo utilizado no relacionamento que estamos implementando
         */
+
+        //usando nomes padronizados
+        // return $this->belongsToMany(Produto::class, 'pedidos_produtos');
+        // juntamente com o model, passamos a tabela auxiliar
     }
 }
