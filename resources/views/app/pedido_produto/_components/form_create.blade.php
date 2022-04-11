6616dc913608e1e5c71995b72e20627f5cf085de
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-10 22:31:16 -0300

    [Refactor] Added quantidade to pedidos-produtos

--- resources/views/app/pedido_produto/_components/form_create.blade.php
+++ resources/views/app/pedido_produto/_components/form_create.blade.php
@@ -10,5 +10,9 @@
         </select>
         {{ $errors->has('produto_id') ? $errors->first('produto_id') : '' }}
     </label>
+    <label>
+        <input type="number" name="quantidade" value="{{ old('quantidade') ? old('quantidade') : '' }}" placeholder="Quantidade" class="borda-preta">
+        {{ $errors->has('quantidade') ? $errors->first('quantidade') : '' }}
+    </label>
     <button type="submit" class="borda-preta">Cadastrar</button>
 </form>
